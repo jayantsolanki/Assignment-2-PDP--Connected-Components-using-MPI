8fafb07e5b23947dabf71bf7121aeec0d0fad737
Jayant <jayantjnp@gmail.com>
2017-11-09 20:35:37 -0500

    assign 2

---
+++ A1/a1.hpp
@@ -0,0 +1,18 @@
+/*  YOUR_FIRST_NAME
+ *  YOUR_LAST_NAME
+ *  YOUR_UBIT_NAME
+ */
+
+#ifndef A1_HPP
+#define A1_HPP
+
+#include <vector>
+#include <mpi.h>
+
+
+int connected_components(std::vector<signed char>& A, int n, int q, const char* out, MPI_Comm comm) {
+    // ...
+    return -1;
+} // connected_components
+
+#endif // A1_HPP
